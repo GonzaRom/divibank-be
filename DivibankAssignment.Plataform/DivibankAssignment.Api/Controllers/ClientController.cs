b3d91cf37b921b3f71b50049d008e233a890fa87
Gonzalo Romero <rom.gonzalo.i@gmail.com>
2021-09-24 18:39:50 -0300

    [DIBANK] Add ClientController and get endpoint
    - Add DI in Startup for ClientService and LoanService

---
+++ DivibankAssignment.Plataform/DivibankAssignment.Api/Controllers/ClientController.cs
@@ -0,0 +1,39 @@
+﻿using DivibankAssignment.Domain.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DivibankAssignment.Domain.Services.Interfaces;
+
+namespace DivibankAssignment.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly ILogger<ClientController> _logger;
+        private readonly IClientService _clientService;
+
+        public ClientController(ILogger<ClientController> logger, IClientService clientService)
+        {
+            _logger = logger;
+            _clientService = clientService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ClientDto> Get(Guid id)
+        {
+            var rng = new Random();
+
+            var client = await new Task<ClientDto>(() => new ClientDto
+            {
+                Birthdate = new DateTime(1988, 11, 09),
+                Id = Guid.NewGuid(),
+                Loans = new List<LoanDto>()
+            });
+
+            return client;
+        }
+    }
+}
---
+++ DivibankAssignment.Plataform/DivibankAssignment.Api/Startup.cs
@@ -0,0 +1,57 @@
+using DivibankAssignment.Domain.Services;
+using DivibankAssignment.Domain.Services.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace DivibankAssignment.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Services
+            services.AddScoped<IClientService, ClientService>();
+            services.AddScoped<ILoanService, LoanService>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DivibankAssignment.Api", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DivibankAssignment.Api v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
